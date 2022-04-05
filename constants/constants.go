421d0dd62773408828692511e598221b9b81c5df
asad_h <asadhussain@lambdatest.com>
2022-04-05 17:19:10 +0530

    working code-docker container app uses kakfa running on host machine(docker desktop mac)

--- constants/constants.go
+++ constants/constants.go
@@ -1,7 +1,11 @@
 package constants
 
-// const BROKERS = "localhost:29092"
-const BROKERS = "broker:9092"
+// const BROKERS = "localhost:9092"
+// const BROKERS = "host.docker.internal:9092"
+// const BROKERS = "localhost:9092,host.docker.internal:9092"
+const BROKERS = "localhost:9092,docker.for.mac.localhost:19092"
+
+// const BROKERS = "broker:9092"
 
 // const BROKERS = "host.docker.internal:9092"
 const TopicName = "asad-sample-topic"
